e5fc3c822e844bc5550b5c103d4ad67a6085a629
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2022-06-07 06:04:03 +0100

    added more tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { useState } from 'react'
+import validator from 'validator'
 import './App.css'
 
 function App() {
+  const [signUpInput, setSignUpInput] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const handleChange = e => {
+    setSignUpInput({
+      ...signUpInput,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const [error, setError] = useState(null)
+
+  const handlClick = e => {
+    e.preventDefault()
+
+    if (!validator.isEmail(signUpInput.email)) {
+      return setError('The Email is invalid!')
+    } else if (signUpInput.password.length < 5) {
+      setError('The Password should contain at least 5 Characters!')
+    } else if (signUpInput.password !== signUpInput.confirmPassword) {
+      setError('The Passwords do not match!')
+    }
+  }
+
   return (
     <div className='container my-5'>
       <form>
@@ -13,6 +42,8 @@ function App() {
             name='email'
             id='email'
             className='form-control'
+            value={signUpInput.email}
+            onChange={handleChange}
           />
         </div>
 
@@ -25,20 +56,34 @@ function App() {
             name='password'
             id='password'
             className='form-control'
+            value={signUpInput.password}
+            onChange={handleChange}
           />
         </div>
 
         <div className='mb-3'>
-          <label htmlFor='confirm-password' className='form-label'>
+          <label htmlFor='confirmPassword' className='form-label'>
             Confirm Password
           </label>
           <input
             type='password'
-            name='confirm-password'
-            id='confirm-password'
+            name='confirmPassword'
+            id='confirmPassword'
             className='form-control'
+            value={signUpInput.confirmPassword}
+            onChange={handleChange}
           />
         </div>
+
+        {error && (
+          <div className='text-danger mb-3'>
+            <small>{error}</small>
+          </div>
+        )}
+
+        <button type='submit' className='btn btn-primary' onClick={handlClick}>
+          Submit
+        </button>
       </form>
     </div>
   )
--- src/App.test.js
+++ src/App.test.js
@@ -3,46 +3,158 @@ import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('Test Form Inputs', () => {
-  test('Email Input value should be empty on initial render', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
     render(<App />)
+  })
+
+  test('Email Input value should be empty on initial render', () => {
     const emailInputElement = screen.getByRole('textbox')
     expect(emailInputElement.value).toBe('')
   })
 
   test('Password Input value should be empty on initial render', () => {
-    render(<App />)
     const passwordInputElement = screen.getByLabelText('Password')
     expect(passwordInputElement.value).toBe('')
   })
 
   test('Confirm Password Input value should be empty on initial render', () => {
-    render(<App />)
     const confirmPasswordInputElement =
       screen.getByLabelText(/confirm password/i)
     expect(confirmPasswordInputElement.value).toBe('')
   })
 
   test('Should be able to type an Email into the Input', () => {
-    render(<App />)
     const emailInputElement = screen.getByRole('textbox', {
       name: /email/i,
     })
-    userEvent.type(emailInputElement, 'eltana"eltana.dev')
-    expect(emailInputElement.value).toBe('eltana"eltana.dev')
+    userEvent.type(emailInputElement, 'eltana@eltana.dev')
+    expect(emailInputElement.value).toBe('eltana@eltana.dev')
   })
 
   test('Should be able to type a Password into the Input', () => {
-    render(<App />)
     const passwordInputElement = screen.getByLabelText('Password')
     userEvent.type(passwordInputElement, 's3cret')
     expect(passwordInputElement.value).toBe('s3cret')
   })
 
   test('Should be able to type a Confirm Password into the Input', () => {
-    render(<App />)
     const confirmPasswordInputElement =
       screen.getByLabelText(/confirm password/i)
     userEvent.type(confirmPasswordInputElement, 's3cret')
     expect(confirmPasswordInputElement.value).toBe('s3cret')
+  })
+
+  test('Should show email error when Invalid email is Typed', () => {
+    const emailErrorElement = screen.queryByText(/The Email is invalid!/i)
+    const emailInputElement = screen.getByRole('textbox', {
+      name: /email/i,
+    })
+    const submitBtnElement = screen.getByRole('button', {
+      name: /submit/i,
+    })
+
+    expect(emailErrorElement).not.toBeInTheDocument()
+
+    // Wrong EMail Test
+    userEvent.type(emailInputElement, 'eltanaeltana.dev')
+    userEvent.click(submitBtnElement)
+
+    const emailErrorElementTwo = screen.queryByText(/The Email is invalid!/i)
+
+    expect(emailErrorElementTwo).toBeInTheDocument()
+  })
+
+  test('Should Show Password Error if Password is less than 5 Characters', () => {
+    const emailInputElement = screen.getByRole('textbox', {
+      name: /email/i,
+    })
+    const passwordInputElement = screen.getByLabelText('Password')
+    const passwordErrorElement = screen.queryByText(
+      /The Password should contain at least 5 Characters!/i
+    )
+    const submitBtnElement = screen.getByRole('button', {
+      name: /submit/i,
+    })
+
+    userEvent.type(emailInputElement, 'eltana@eltana.dev')
+    expect(passwordErrorElement).not.toBeInTheDocument()
+
+    userEvent.type(passwordInputElement, '123')
+    userEvent.click(submitBtnElement)
+
+    const passwordErrorElementTwo = screen.queryByText(
+      /The Password should contain at least 5 Characters!/i
+    )
+
+    expect(passwordErrorElementTwo).toBeInTheDocument()
+  })
+
+  test('Should Show Confirm Password does not Match Password', () => {
+    const emailInputElement = screen.getByRole('textbox', {
+      name: /email/i,
+    })
+    const passwordInputElement = screen.getByLabelText('Password')
+    const confirmPasswordErrorElement = screen.queryByText(
+      /The Passwords do not match!/i
+    )
+    const confirmPasswordInputElement =
+      screen.getByLabelText(/confirm password/i)
+
+    const submitBtnElement = screen.getByRole('button', {
+      name: /submit/i,
+    })
+    userEvent.click(submitBtnElement)
+
+    userEvent.type(emailInputElement, 'eltana@eltana.dev')
+    userEvent.type(passwordInputElement, 's3cret')
+
+    expect(confirmPasswordErrorElement).not.toBeInTheDocument()
+
+    // WRONG PASSWORD
+    userEvent.type(confirmPasswordInputElement, '12345')
+
+    userEvent.click(submitBtnElement)
+
+    const confirmPasswordErrorElementTwo = screen.queryByText(
+      /The Passwords do not match!/i
+    )
+
+    expect(confirmPasswordErrorElementTwo).toBeInTheDocument()
+  })
+
+  test('Should Show No Error if Password If all Input Fields are Valid', () => {
+    const emailInputElement = screen.getByRole('textbox', {
+      name: /email/i,
+    })
+    const passwordInputElement = screen.getByLabelText('Password')
+    const confirmPasswordInputElement =
+      screen.getByLabelText(/confirm password/i)
+    const submitBtnElement = screen.getByRole('button', {
+      name: /submit/i,
+    })
+
+    userEvent.type(emailInputElement, 'eltana@eltana.dev')
+    userEvent.type(passwordInputElement, 's3cret')
+    userEvent.type(confirmPasswordInputElement, 's3cret')
+
+    userEvent.click(submitBtnElement)
+
+    const emailErrorElement = screen.queryByText(/The Email is invalid!/i)
+    const passwordErrorElement = screen.queryByText(
+      /The Password should contain at least 5 Characters!/i
+    )
+    const confirmPasswordErrorElementTwo = screen.queryByText(
+      /The Passwords do not match!/i
+    )
+
+    // expect(emailErrorElement).not.toBeInTheDocument()
+    // expect(passwordErrorElement).not.toBeInTheDocument()
+    // expect(confirmPasswordErrorElementTwo).not.toBeInTheDocument()
+    expect(
+      emailErrorElement ||
+        passwordErrorElement ||
+        confirmPasswordErrorElementTwo
+    ).not.toBeInTheDocument()
   })
 })
